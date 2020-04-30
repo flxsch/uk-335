42057553c59701c2be6ff7c25d6c1938d78b9554
flxsch <felix.schneider@edu.tbz.ch>
2020-04-30 15:56:08 +0200

    main menu UI initial commit

---
+++ app/src/main/java/ch/noseryoung/uek335/MainMenuActivity.java
@@ -0,0 +1,22 @@
+package ch.noseryoung.uek335;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
